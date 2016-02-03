ba61bf3f8169fa6e833a3124c5bb491ae9888d0e
dlawr <dan.lawrence3@gmail.com>
2016-02-03 14:33:15 -0500

    testEdge now uses multi dimentional arrays

--- vertex_demo/scripts.js
+++ vertex_demo/scripts.js
@@ -28,38 +28,55 @@ board = {
 
 function Vertex() {
   this.color = 'blank';
-
+  this.edge = [];
 }
 
 function testEdge(arr) {
-  var match = false;
-  if (arr[0].color != 'blank') {
-    match = true;
-    var current = arr[0].color;
-    for (var i = 0; i < arr.length; i++) {
-      if (current != arr[i].color) {
-        match = false;
+  for (var i = 0; i < arr.length; i++) {
+    var match = false;
+    if (arr[i][0].color != 'blank') {
+      match = true;
+      var current = arr[i][0].color;
+      for (var j = 0; j < arr.length; j++) {
+        if (current != arr[i][j].color) {
+          match = false;
+        }
       }
     }
   }
   return match;
 }
 
-function addClickEvent(node) {
+function addClickEvent($node, fn) {
+  $node.on('click', fn);
+}
+
+function removeClickEvent($node) {
 
 }
 
-function removeClickEvent(node) {
+function makeBlack($node) {
+  $node.color = 'black';
+  $node.attr('color', 'black');
+}
 
+function makeWhite($node) {
+  $node.color = 'white';
+  $($node).css( "color", "red" );
+}
+
+function makeBlank($node) {
+  $node.color = 'blank';
+  $node.attr('color', 'black');
 }
 
 var vertex1 = new Vertex();
 var vertex2 = new Vertex();
 var vertex3 = new Vertex();
 
-vertex1.edge = [vertex1, vertex2, vertex3];
-vertex2.edge = [vertex1, vertex2, vertex3];
-vertex3.edge = [vertex1, vertex2, vertex3];
+vertex1.edge.push([vertex1, vertex2, vertex3]);
+vertex2.edge.push([vertex1, vertex2, vertex3]);
+vertex3.edge.push([vertex1, vertex2, vertex3]);
 
 vertex1.adjacent = [vertex2];
 vertex2.adjacent = [vertex1, vertex3];
@@ -81,3 +98,7 @@ console.log(vertex3.edge);
 console.log(testEdge(vertex3.edge));
 
 console.log(vertex3);
+
+$( "#two" ).on( "mouseover", function() {
+  $( "#two" ).css( "color", "red" );
+});
