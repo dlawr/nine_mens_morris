4a680bd3297b9c60b646bd05f74b03e5e41cf888
dlawr <dan.lawrence3@gmail.com>
2016-02-03 11:51:49 -0500

    linked jquery and scripts.js

--- vertex_demo/scripts.js
+++ vertex_demo/scripts.js
@@ -1,17 +1,57 @@
 vertex1 = {
-  color: 'blank',
+  id: $('#one'),
+  color: 'blank', //blank black white
   adjacent: [vertex2],
   edge: [vertex1, vertex2, vertex3]
 }
 
 vertex2 = {
+  id: $('#two'),
+  color: 'blank',
+  adjacent: [vertex1, vertex3],
+  edge: [vertex1, vertex2, vertex3]
+}
+
+vertex3 = {
+  id: $('#three'),
   color: 'blank',
   adjacent: [vertex2],
   edge: [vertex1, vertex2, vertex3]
 }
 
-vertex3 = {
-  color: 'blank',
-  adjacent: [vertex2],
+board = {
+  turn: '',
+  action: 'place', //place capture select move
+
   edge: [vertex1, vertex2, vertex3]
 }
+
+function testEdge(arr) {
+  var match = false;
+  if (arr[0] != 'blank') {
+    match = true;
+    var current = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+      if (current != arr[i]) {
+        match = false;
+      }
+    }
+  }
+  return match;
+}
+
+function addClickEvent(node) {
+
+}
+
+function removeClickEvent(node) {
+
+}
+
+console.log(edgeTest(vertex3.edge));
+
+vertex1.color = 'black';
+vertex2.color = 'black';
+vertex3.color = 'black';
+
+console.log(edgeTest(vertex3.edge));
