db40ce2bd47dba916c11ca22c861e507c8ab5b01
dlawr <dan.lawrence3@gmail.com>
2016-02-03 12:50:54 -0500

    scripts.js loads in browser without errors, bugfix in testEdge function

--- vertex_demo/scripts.js
+++ vertex_demo/scripts.js
@@ -1,38 +1,43 @@
-vertex1 = {
-  id: $('#one'),
-  color: 'blank', //blank black white
-  adjacent: [vertex2],
-  edge: [vertex1, vertex2, vertex3]
-}
-
-vertex2 = {
-  id: $('#two'),
-  color: 'blank',
-  adjacent: [vertex1, vertex3],
-  edge: [vertex1, vertex2, vertex3]
-}
-
-vertex3 = {
-  id: $('#three'),
-  color: 'blank',
-  adjacent: [vertex2],
-  edge: [vertex1, vertex2, vertex3]
-}
+// vertex1 = {
+//   id: $('#one'),
+//   color: 'blank', //blank black white
+//   adjacent: [vertex2],
+//   edge: [vertex1, vertex2, vertex3]
+// }
+//
+// vertex2 = {
+//   id: $('#two'),
+//   color: 'blank',
+//   adjacent: [vertex1, vertex3],
+//   edge: [vertex1, vertex2, vertex3]
+// }
+//
+// vertex3 = {
+//   id: $('#three'),
+//   color: 'blank',
+//   adjacent: [vertex2],
+//   edge: [vertex1, vertex2, vertex3]
+// }
 
 board = {
   turn: '',
   action: 'place', //place capture select move
 
-  edge: [vertex1, vertex2, vertex3]
+  // edge: [vertex1, vertex2, vertex3]
+}
+
+function Vertex() {
+  this.color = 'blank';
+
 }
 
 function testEdge(arr) {
   var match = false;
-  if (arr[0] != 'blank') {
+  if (arr[0].color != 'blank') {
     match = true;
-    var current = arr[0];
+    var current = arr[0].color;
     for (var i = 0; i < arr.length; i++) {
-      if (current != arr[i]) {
+      if (current != arr[i].color) {
         match = false;
       }
     }
@@ -48,10 +53,31 @@ function removeClickEvent(node) {
 
 }
 
-console.log(edgeTest(vertex3.edge));
+var vertex1 = new Vertex();
+var vertex2 = new Vertex();
+var vertex3 = new Vertex();
+
+vertex1.edge = [vertex1, vertex2, vertex3];
+vertex2.edge = [vertex1, vertex2, vertex3];
+vertex3.edge = [vertex1, vertex2, vertex3];
+
+vertex1.adjacent = [vertex2];
+vertex2.adjacent = [vertex1, vertex3];
+vertex3.adjacent = [vertex2];
+
+vertex1.id = $('#one');
+vertex2.id = $('#two');
+vertex3.id = $('#three');
+
+
+console.log(testEdge(vertex3.edge));
 
 vertex1.color = 'black';
 vertex2.color = 'black';
 vertex3.color = 'black';
 
-console.log(edgeTest(vertex3.edge));
+
+console.log(vertex3.edge);
+console.log(testEdge(vertex3.edge));
+
+console.log(vertex3);
