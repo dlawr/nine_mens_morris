27d2ca57bf3ccf79376e769c0c3de37154559b51
dlawr <dan.lawrence3@gmail.com>
2016-02-03 10:50:12 -0500

    bare bones layout for html file and vertex objects

---
+++ vertex_demo/scripts.js
@@ -0,0 +1,17 @@
+vertex1 = {
+  color: 'blank',
+  adjacent: [vertex2],
+  edge: [vertex1, vertex2, vertex3]
+}
+
+vertex2 = {
+  color: 'blank',
+  adjacent: [vertex2],
+  edge: [vertex1, vertex2, vertex3]
+}
+
+vertex3 = {
+  color: 'blank',
+  adjacent: [vertex2],
+  edge: [vertex1, vertex2, vertex3]
+}
