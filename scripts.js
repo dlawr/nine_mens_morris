7eaee9f9fc978087a14ad3da233f2f47ba1843e1
dlawr <dan.lawrence3@gmail.com>
2016-02-04 11:19:41 -0500

    assigned vertex objects to corrosponding nodes.

--- scripts.js
+++ scripts.js
@@ -29,35 +29,44 @@ function testEdge(arr) {
 
 function createVerticies() {
   for (var i = 0; i < 24; i++) {
-    board.verticies.push(new Vertex(i)); //game does not use position of vertex in array only edge and adjascent stored within vertex
+    board.verticies.push(new Vertex(i)); //game does not use position of vertex in array only edge and adjacent stored within vertex
   }
 
   for (var i = 0; i < 24; i++) {
     if (i % 8 === 0) {
       board.verticies[i].edge.push([board.verticies[i], board.verticies[i+1], board.verticies[i+2]]);
       board.verticies[i].edge.push([board.verticies[i], board.verticies[i+7], board.verticies[i+6]]);
-      board.verticies[i].adjascent = [board.verticies[i+7], board.verticies[i+1]];
+      board.verticies[i].adjacent = [board.verticies[i+7], board.verticies[i+1]];
     } else if (i % 8 === 7) {
       board.verticies[i].edge.push([board.verticies[i-1], board.verticies[i], board.verticies[i-7]]);
       board.verticies[i].edge.push([board.verticies[7], board.verticies[15], board.verticies[23]]);
-      board.verticies[i].adjascent = [board.verticies[i-7], board.verticies[i-1]];
+      board.verticies[i].adjacent = [board.verticies[i-7], board.verticies[i-1]];
     } else if (i % 2 === 0) {
       board.verticies[i].edge.push([board.verticies[i], board.verticies[i+1], board.verticies[i+2]]);
       board.verticies[i].edge.push([board.verticies[i], board.verticies[i-1], board.verticies[i-2]]);
-      board.verticies[i].adjascent = [board.verticies[i-1], board.verticies[i+1]];
+      board.verticies[i].adjacent = [board.verticies[i-1], board.verticies[i+1]];
     } else if (i < 7) {
       board.verticies[i].edge.push([board.verticies[i-1], board.verticies[i], board.verticies[i+1]]);
       board.verticies[i].edge.push([board.verticies[i], board.verticies[i+8], board.verticies[i+16]]);
-      board.verticies[i].adjascent = [board.verticies[i-1], board.verticies[i+1], board.verticies[i+8]];
+      board.verticies[i].adjacent = [board.verticies[i-1], board.verticies[i+1], board.verticies[i+8]];
     } else if (i < 15) {
       board.verticies[i].edge.push([board.verticies[i-1], board.verticies[i], board.verticies[i+1]]);
       board.verticies[i].edge.push([board.verticies[i-8], board.verticies[i], board.verticies[i+8]]);
-      board.verticies[i].adjascent = [board.verticies[i-1], board.verticies[i+1], board.verticies[i-8], board.verticies[i+8]];
+      board.verticies[i].adjacent = [board.verticies[i-1], board.verticies[i+1], board.verticies[i-8], board.verticies[i+8]];
     } else {
       board.verticies[i].edge.push([board.verticies[i-1], board.verticies[i], board.verticies[i+1]]);
       board.verticies[i].edge.push([board.verticies[i], board.verticies[i-8], board.verticies[i-16]]);
-      board.verticies[i].adjascent = [board.verticies[i-1], board.verticies[i+1], board.verticies[i-8]];
+      board.verticies[i].adjacent = [board.verticies[i-1], board.verticies[i+1], board.verticies[i-8]];
     }
   }
 
+}
+
+function assignVerticies() {
+  for (var i = 0; i < board.verticies.length; i++) {
+    // var i = 0;
+    var id = '';
+    id = 'vertex' + i;
+    board.verticies[i].id = $(id);
+  }
 }
